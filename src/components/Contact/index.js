a83383c890d0a0a8bc5d59fd124046009a106f7d
Steve M <uziandari@gmail.com>
2017-02-03 09:42:30 -0500

    begun contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,16 +5,23 @@ import Nav from '../Nav/index';
 import './style';
 
 export default class Contact extends Component {
+  constructor() {
+    super();
+  }
 
   render() {
-    return (
+    return(
       <div>
         <Nav />
         <div id="contact-wrapper">
-          <h1>Contact</h1>
+          <form className='react-form' /*onSubmit={this.handleSubmit}*/>
+            <h1>Say Hi!</h1>       
+          </form>
         </div>
       </div>
-    );
+    )
   }
 
+
+ 
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import './style';
 
 export default class Project extends Component {
 	
 	render() {
-		var projectsNode = this.props.projects.map((project) => 
+
+		var projectsNode = this.props.projects.map((project) =>
+
 			<li key={project.id} className="list-col">
 				<h3>{project.name}</h3>
 				<p className="description">{project.desc}</p>
