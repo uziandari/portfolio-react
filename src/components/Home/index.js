a70115e1e113baaf0832898b6fe16be8963e9e2a
Steve M <uziandari@gmail.com>
2017-02-07 07:40:52 -0500

    fixed external links

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -17,8 +17,8 @@ export default class Home extends Component {
         <div className="links">
           <a href="https://github.com/uziandari"><i className="fa fa-github-alt" aria-hidden="true"></i></a>
           <a href="https://twitter.com/SteveMessing"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-          <a href="https://github.com/uziandari"><i className="fa fa-free-code-camp" aria-hidden="true"></i></a>
-          <a href="https://github.com/uziandari"><i className="fa fa-codepen" aria-hidden="true"></i></a>      
+          <a href="https://freecodecamp.com/uziandari"><i className="fa fa-free-code-camp" aria-hidden="true"></i></a>
+          <a href="https://codepen.io/uziandari"><i className="fa fa-codepen" aria-hidden="true"></i></a>      
         </div>
       </div>
     );
